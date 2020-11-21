54c4f5b1cbee5a38e2eddbf8758b56871ef1caa3
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2020-11-21 20:57:56 +0530

    Create Remove_duplicates_from_an_unsorted_linked_list.cpp
    Given an unsorted linked list of N nodes. The task is to remove duplicate elements from this unsorted Linked List. When a value appears in multiple nodes, the node which appeared first should be kept, all others duplicates are to be removed.
    
    Example 1:
    
    Input:
    N = 4
    value[] = {5,2,2,4}
    Output: 5 2 4
    Explanation:Given linked list elements are
    5->2->2->4, in which 2 is repeated only.
    So, we will delete the extra repeated
    elements 2 from the linked list and the
    resultant linked list will contain 5->2->4
    Example 2:
    
    Input:
    N = 5
    value[] = {2,2,2,2,2}
    Output: 2
    Explanation:Given linked list elements are
    2->2->2->2->2, in which 2 is repeated. So,
    we will delete the extra repeated elements
    2 from the linked list and the resultant
    linked list will contain only 2.
    Your Task:
    You have to complete the method removeDuplicates() which takes 1 argument: the head of the linked list.  Your function should return a pointer to a linked list with no duplicate element.
    
    
    https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1/?category[]=Linked%20List&company[]=Amazon&problemStatus=solved&problemType=functional&difficulty[]=0&page=1&sortBy=submissions&query=category[]Linked%20Listcompany[]AmazonproblemStatussolvedproblemTypefunctionaldifficulty[]0page1sortBysubmissions

---
+++ Linked_List/Remove_duplicates_from_an_unsorted_linked_list.cpp
@@ -0,0 +1,100 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+Node* removeDuplicates(Node *root);
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+// Function to remove duplicates from the linked list
+// root: root of the linked list
+#include<bits/stdc++.h>
+
+Node * removeDuplicates( Node *head) 
+{
+ // your code goes here
+    unordered_set <int> s;    
+    Node *current  = head;
+    Node *prev = NULL;
+    while(current !=NULL)
+    {
+        if(s.find(current->data )!= s.end())
+        {
+            prev->next = current->next;
+            delete(current);
+            
+        }
+        else
+        {
+            s.insert(current->data);
+            prev = current;
+        }
+        current = prev->next;
+    }
+        return head;
+}
+
