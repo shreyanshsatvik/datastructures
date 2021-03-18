02b1d9d6d793c8f8b44ee1a21109329f8d641836
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-03-19 00:07:08 +0530

    Update longest_subsequence.cpp
    Input: arr[] = {3, 10, 2, 1, 20}
    Output: Length of LIS = 3
    The longest increasing subsequence is 3, 10, 20
    
    Input: arr[] = {3, 2}
    Output: Length of LIS = 1
    The longest increasing subsequences are {3} and {2}
    
    Input: arr[] = {50, 3, 10, 7, 40, 80}
    Output: Length of LIS = 4
    The longest increasing subsequence is {3, 7, 40, 80}

--- dynamic/longest_subsequence.cpp
+++ dynamic/longest_subsequence.cpp
@@ -1,1 +1,46 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int max_subsequence(vector<int> &arr)
+{
+    int p =arr.size();
+    vector<int> l(p,1);
+    for(int i=1;i<arr.size();i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(arr[i]>arr[j] && l[i]<l[j]+1)
+            {
+                l[i] = l[j]+1;
+            }
+        }
+    }
+
+    return *max_element(l.begin(),l.end());
+
+
+
+
+
+}
+
+int main()
+{
+    cout<<"Enter Number of Elements:"<<endl;
+    int n;
+    cin>>n;
+    cout<<"Enter array:"<<endl;
+    vector<int> arr;
+    while(n--)
+    {
+        int k;
+        cin>>k;
+        arr.push_back(k);
+    }
+    int l = max_subsequence(arr);
+    cout<<"Maximum Subsequence Length:- "<<endl;
+    cout<<l<<endl;
+
+
+}
