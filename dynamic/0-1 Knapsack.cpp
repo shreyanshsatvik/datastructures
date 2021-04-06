0ce352b50303a2d02a9c3b274d6541724d96eae6
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-04-06 20:26:47 +0530

    Update 0-1 Knapsack.cpp
    You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
    In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
    
    Example 1:
    
    Input:
    N = 3
    W = 4
    values[] = {1,2,3}
    weight[] = {4,5,1}
    Output: 3
    Example 2:
    
    Input:
    N = 3
    W = 3
    values[] = {1,2,3}
    weight[] = {4,5,6}
    Output: 0
    Your Task:
    Complete the function knapSack() which takes maximum capacity W, weight array wt[], value array val[] and number of items n as a parameter and returns the maximum possible value you can get.
    
    Expected Time Complexity: O(N*W).
    Expected Auxiliary Space: O(N*W)
    
    Constraints:
    1 ≤ N ≤ 1000
    1 ≤ W ≤ 1000
    1 ≤ wt[i] ≤ 1000
    1 ≤ v[i] ≤ 1000

--- dynamic/0-1 Knapsack.cpp
+++ dynamic/0-1 Knapsack.cpp
@@ -1,24 +1,76 @@
-def knapSack(W, wt, val, n):
-	K = [[0 for x in range(W + 1)] for x in range(n + 1)]
-
-	# Build table K[][] in bottom up manner
-	for i in range(n + 1):
-		for w in range(W + 1):
-			if i == 0 or w == 0:
-				K[i][w] = 0
-			elif wt[i-1] <= w:
-				K[i][w] = max(val[i-1]
-						+ K[i-1][w-wt[i-1]],
-							K[i-1][w])
-			else:
-				K[i][w] = K[i-1][w]
-
-	print(K ,end="") 
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
 
 
-# Driver code
-val = [60, 100, 120]
-wt = [1, 2, 3]
-W = 5
-n = len(val)
-print(knapSack(W, wt, val, n))
+ // } Driver Code Ends
+
+
+// Returns the maximum value that  
+// can be put in a knapsack of capacity W 
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+   // Your code here
+   vector<vector<int>> arr(n+1,vector<int>(W+1));
+   
+   for(int i =0;i<n+1;i++)
+   {
+       for(int k=0;k<W+1;k++)
+       {
+           if(i==0 || k==0)
+           {
+               arr[i][k] = 0;
+           }
+           else if(k>= wt[i-1] )
+           {
+               arr[i][k] = max(arr[i-1][k] , arr[i-1][k-wt[i-1]]+val[i-1]);
+               
+           }
+           else
+           {
+               arr[i][k] = arr[i-1][k];
+           }
+       }
+       
+       
+       
+   }
+   return arr[n][W];
+   
+   
+}
+
+
+
+
+
+// { Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        
+        //calling method knapSack()
+        cout<<knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
