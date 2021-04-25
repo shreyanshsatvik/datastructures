4dd7a364a0af8156132c3b902126cd3c94d0883f
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-04-25 22:06:26 +0530

    Create Reverse a Linked List in groups of given size.cpp
    Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list.
    
    Example 1:
    
    Input:
    LinkedList: 1->2->2->4->5->6->7->8
    K = 4
    Output: 4 2 2 1 8 7 6 5 
    Explanation: 
    The first 4 elements 1,2,2,4 are reversed first 
    and then the next 4 elements 5,6,7,8. Hence, the 
    resultant linked list is 4->2->2->1->8->7->6->5.
    Example 2:
    
    Input:
    LinkedList: 1->2->3->4->5
    K = 3
    Output: 3 2 1 5 4 
    Explanation: 
    The first 3 elements are 1,2,3 are reversed 
    first and then elements 4,5 are reversed.Hence, 
    the resultant linked list is 3->2->1->5->4.
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function reverse() which should reverse the linked list in group of size k and return the head of the modified linked list.
    
    Expected Time Complexity : O(N)
    Expected Auxilliary Space : O(1)
    
    Constraints:
    1 <= N <= 104
    1 <= k <= N
    Company Tags
    Topic Tags
    Related Interview Experiences
    
    If you are facing any issue on this page. Please let us know.
    
    Output Window

---
+++ Linked_List/Reverse a Linked List in groups of given size.cpp
@@ -0,0 +1,29 @@
+struct node *reverse (struct node *head, int k)
+    { 
+        if(!head)
+        {
+            return NULL;
+        }
+        node* current = head;
+        node* next = NULL;
+        node* prev = NULL;
+        int count = 0;
+ 
+    /*reverse first k nodes of the linked list */
+    while (current != NULL && count < k) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+        count++;
+    }
+ 
+    /* next is now a pointer to (k+1)th node
+    Recursively call for the list starting from current.
+    And make rest of the list as next of first node */
+    if (next != NULL)
+        head->next = reverse(next, k);
+ 
+    /* prev is new head of the input list */
+    return prev;
+}
