2b685839060c1234fa3820677569059dc1049b55
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-05-04 11:56:52 +0530

    Create LCS of three strings.cpp
    LCS of three strings 
    Medium Accuracy: 65.09% Submissions: 1793 Points: 4
    Given 3 strings A, B and C, the task is to find the longest common sub-sequence in all three given sequences.
    
    Example 1:
    
    Input:
    A = "geeks", B = "geeksfor", 
    C = "geeksforgeeks"
    Output: 5
    Explanation: "geeks"is the longest common
    subsequence with length 5.
    â€‹Example 2:
    
    Input: 
    A = "abcd", B = "efgh", C = "ijkl"
    Output: 0
    Explanation: There's no common subsequence
    in all the strings.
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function LCSof3() which takes the strings A, B, C and their lengths n1, n2, n3 as input and returns the length of the longest common subsequence in all the 3 strings.
    
    
    Expected Time Complexity: O(n1*n2*n3).
    Expected Auxiliary Space: O(n1*n2*n3).
    
    
    Constraints:
    1<=n1, n2, n3<=100
    
    Company Tags
    Topic Tags
    Related Interview Experiences
    
    If you are facing any issue on this page. Please let us know.
    
    Output Window

---
+++ dynamic/LCS of three strings.cpp
@@ -0,0 +1,42 @@
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
+{
+    // your code here
+    if(A.size()==0 || B.size()==0 || C.size()==0)
+    {
+        return 0;
+    }
+
+    
+    vector<vector<vector<int>>> arr(n1+1,vector<vector<int>>(n2+1,vector<int>(n3+1)));
+
+    for(int i=0;i<n1+1;i++)
+    {
+        for(int j=0;j<n2+1;j++)
+        {
+            for(int k=0;k<n3+1;k++)
+            {
+                if(i==0 || j==0 || k==0)
+                {
+                    arr[i][j][k] = 0;
+                }
+                else if(A[i-1]==B[j-1] && A[i-1]==C[k-1])
+                {
+                    arr[i][j][k] = arr[i-1][j-1][k-1]+1;
+                }
+                else
+                {
+                    int a = max(arr[i-1][j][k],arr[i][j-1][k]);
+                    arr[i][j][k]= max(a,arr[i][j][k-1]);
+                }
+            }
+            
+            
+        }
+    }
+   
+    return arr[n1][n2][n3];
+    
+    
+    
+    
+}
