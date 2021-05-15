51f46c26bddc283c98b20ac8380bf5b7535eebf0
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-05-15 13:24:34 +0530

    Create Number of 1 Bits.cpp
    Given a positive integer N, print count of set bits in it. 
    
    Example 1:
    
    Input:
    N = 6
    Output:
    2
    Explanation:
    Binary representation is '110' 
    So the count of the set bit is 2.
    Example 2:
    
    Input:
    8
    Output:
    1
    Explanation:
    Binary representation is '1000' 
    So the count of the set bit is 1.
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function setBits() which takes an Integer N and returns the count of number of set bits.
    
    Expected Time Complexity: O(LogN)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    1 ≤ N ≤ 109
    
    Company Tags
    Topic Tags
    Related Interview Experiences
    
    If you are facing any issue on this page. Please let us know.
    
    Output Window

---
+++ Bit-Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int count = 0;
+        while(N>0)
+        {
+            N &= (N-1);
+            count++;
+        }
+        return count;
+    }
+};
