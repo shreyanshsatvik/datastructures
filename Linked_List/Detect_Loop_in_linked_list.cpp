8e052dd931ee94307f375139c3d65167de98790e
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-07-03 18:34:28 +0530

    Update Detect_Loop_in_linked_list.cpp
    Given head, the head of a linked list, determine if the linked list has a cycle in it.
    
    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
    
    Return true if there is a cycle in the linked list. Otherwise, return false.
    
     
    
    Example 1:
    
    
    Input: head = [3,2,0,-4], pos = 1
    Output: true
    Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
    Example 2:
    
    
    Input: head = [1,2], pos = 0
    Output: true
    Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
    Example 3:
    
    
    Input: head = [1], pos = -1
    Output: false
    Explanation: There is no cycle in the linked list.
     
    
    Constraints:
    
    The number of the nodes in the list is in the range [0, 104].
    -105 <= Node.val <= 105
    pos is -1 or a valid index in the linked-list.
     
    
    Follow up: Can you solve it using O(1) (i.e. constant) memory?
    
    Accepted
    966,976
    Submissions
    2,220,470

--- Linked_List/Detect_Loop_in_linked_list.cpp
+++ Linked_List/Detect_Loop_in_linked_list.cpp
@@ -1,98 +1,25 @@
-// { Driver Code Starts
-// driver code
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-    int data;
-    Node* next;
-    
-    Node(int val)
-    {
-        data = val;
-        next = NULL;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
-
-void loopHere(Node* head, Node* tail, int position)
-{
-    if(position==0) return;
-    
-    Node* walk = head;
-    for(int i=1; i<position; i++)
-        walk = walk->next;
-    tail->next = walk;
-}
-
-bool detectLoop(Node* head);
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n, num;
-        cin>>n;
-        
-        Node *head, *tail;
-        cin>> num;
-        head = tail = new Node(num);
-        
-        for(int i=0 ; i<n-1 ; i++)
-        {
-            cin>> num;
-            tail->next = new Node(num);
-            tail = tail->next;
-        }
-        
-        int pos;
-        cin>> pos;
-        loopHere(head,tail,pos);
-        
-        if( detectLoop(head) )
-            cout<< "True\n";
-        else
-            cout<< "False\n";
-    }
-	return 0;
-}
-// } Driver Code Ends
-
-
-/*
-
-struct Node
-{
-    int data;
-    struct Node *next;
-    int flag;
-    Node(int x) {
-        data = x;
-        next = NULL;
-    
-    }
-
-*/
-
-bool detectLoop(Node* head)
-{
-    // your code here
-    vector<Node*> s;
-    while(head!=NULL)
-    {
-
-        if(binary_search(s.begin(),s.end(),head))
-        {
-            return true;
-        }
-        s.push_back(head);
-        head = head->next;
-    }
-    return false;
-    
-    
-    
-}
