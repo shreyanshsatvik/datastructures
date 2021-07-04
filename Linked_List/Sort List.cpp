988e5b6747ab8fd8815c3d11b2e5b9543ed0743e
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-07-04 09:59:47 +0530

    Create Sort List.cpp
    Given the head of a linked list, return the list after sorting it in ascending order.
    
    Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
    
     
    
    Example 1:
    
    
    Input: head = [4,2,1,3]
    Output: [1,2,3,4]
    Example 2:
    
    
    Input: head = [-1,5,3,4,0]
    Output: [-1,0,3,4,5]
    Example 3:
    
    Input: head = []
    Output: []
     
    
    Constraints:
    
    The number of nodes in the list is in the range [0, 5 * 104].
    -105 <= Node.val <= 105
    Accepted
    368,830
    Submissions
    774,786

---
+++ Linked_List/Sort List.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findmiddle(ListNode* head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* merge(ListNode* head1,ListNode* head2)
+    {
+        ListNode* merged = new ListNode(-1);
+        ListNode* temp = merged;
+        
+        while(head1 && head2)
+        {
+            if(head1->val<head2->val)
+            {
+                temp->next= head1;
+                head1 = head1->next;
+                temp = temp->next;
+            }
+            else
+            {
+                temp->next= head2;
+                head2 = head2->next;
+                temp = temp->next;
+            }
+        }
+        if(head1)
+        {
+            temp->next = head1;
+            
+        }
+        if(head2)
+        {
+            temp->next = head2;
+        }
+        return merged->next;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        ListNode* middle = findmiddle(head);
+        ListNode* temp = middle->next;
+        middle->next = NULL;
+        ListNode* first_half = sortList(head);
+        ListNode* second_half = sortList(temp);
+        ListNode* merged = merge(first_half,second_half);
+        return merged;
+        
+    }
+};
