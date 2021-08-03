acc144d82647e5930b03698448423226b4270387
Shreyansh Satvik <52126313+shreyanshsatvik@users.noreply.github.com>
2021-08-03 11:23:48 +0530

    Update Remove K Digits.cpp
    Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
    
     
    
    Example 1:
    
    Input: num = "1432219", k = 3
    Output: "1219"
    Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
    Example 2:
    
    Input: num = "10200", k = 1
    Output: "200"
    Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
    Example 3:
    
    Input: num = "10", k = 2
    Output: "0"
    Explanation: Remove all the digits from the number and it is left with nothing which is 0.

--- greedy/Remove K Digits.cpp
+++ greedy/Remove K Digits.cpp
@@ -11,7 +11,8 @@ string removeKdigits(string num, int k) {
         }
         // trim leading zeros
         int s = 0;
-        while (s<(int)num.size()-1 && num[s]=='0')  s++;
+        while (s<(int)num.size()-1 && num[s]=='0') 
+                s++;
         num.erase(0, s);
         
         return num=="" ? "0" : num;
